ff1e318706300330e9ff5fd133fe70f715b9f7b9
Jiselle Martins <jisellevms@gmail.com>
2024-03-04 20:39:42 -0300

    primeira configuracao aplicacoes

---
+++ busca/src/main/java/com/jisellemartins/arq/busca/BuscaApplication.java
@@ -0,0 +1,13 @@
+package com.jisellemartins.arq.busca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscaApplication.class, args);
+	}
+
+}
